603e7988d02649295a8eea4bdc9e27c24564a0ad
projektant-pata <richard.hyvl@gmail.com>
2025-01-09 16:20:50 +0100

    coSeZivotem

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\GameApiController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('/', function () {
-    return response()->json(['organization' => 'Student Cyber Games']);
+Route::prefix('/v1')->group(function () {
+
+    Route::get('/', function () {
+        return response()->json(['organization' => 'Student Cyber Games']);
+    });
+    Route::get('/games', [GameApiController::class, 'getAll']);
+    Route::post('/games', [GameApiController::class, 'new']);
+    Route::get('/games/{uuid}', [GameApiController::class, 'get']);
+    Route::put('/games/{uuid}', [GameApiController::class, 'update']);
+    Route::delete('/games/{uuid}', [GameApiController::class, 'remove']);
 });
